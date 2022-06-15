9ea4b00ac34201aeb368c20ec50ded0341073567
Sourabh Sethi <sourabhhsethii@linux.com>
2022-06-14 22:08:19 -0400

    Arrays Part 2

--- basic/array.js
+++ basic/array.js
@@ -1,3 +1,7 @@
+/**
+ * Arrays Part 1
+ */
+
 let arr = [1,2,3];
 console.log(arr[1])
 
@@ -11,3 +15,21 @@ for(let i=0;i< arr2.length;i++){
 for (let index in arr2){
     console.log(index);
 }
+
+/**
+ * Arrays Part 2
+ */
+
+let fruits = ["apple", "mango"];
+console.log(fruits);
+// Push & Pop
+fruits.push("mellon");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+// shift & unshift
+fruits.shift();
+console.log(fruits);
+fruits.unshift("kiwi", "grapes");
+console.log(fruits);
