c03fc620c460e719c1fc50dd4932597fe0762e9c
Sourabh Sethi <sourabhhsethii@linux.com>
2022-06-14 22:22:59 -0400

    basic javascript code added.

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+Javascript is sync single-thread language. 
+
+## Excuction Context
+
---
+++ basic/basic.js
@@ -0,0 +1,11 @@
+var n =2;
+function square(num) {
+    var ans = num * num;
+    return ans;
+}
+
+var square2 = square(n);
+var square3 = square(4);
+
+console.log(square2);
+console.log(square3);
