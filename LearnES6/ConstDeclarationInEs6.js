6c001bf61d497e6c4ce247fa18077b63dcca3cb4
Sourabh Sethi <sourabhhsethii@linux.com>
2022-06-28 23:02:03 -0400

    ES6 Const

---
+++ LearnES6/ConstDeclarationInEs6.js
@@ -0,0 +1,13 @@
+/**
+ * Const Declaration In Es6
+ */
+
+const value = 'hello World';
+// value = '10000'; // Not Valid - TypeError: Assignment to constant variable.
+
+const obj = {
+    name : 'sourabh'
+}
+// Values inside the object can be updated but not value itself.
+obj.lastname = 'sethi'
+console.log(obj);
