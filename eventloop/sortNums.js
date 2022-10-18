c65a200cd74c5d775d11fafd2b6848388978e027
Sourabh Sethi <sourabhhsethii@linux.com>
2022-10-18 01:01:45 -0400

    Howework

---
+++ eventloop/sortNums.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+/**
+ * BASIC
+ * 1.) Read the number from file1.txt
+ * 2.) sort them in ascending order
+ * 3.) write the sorted numbers to do out.txt
+ */
+
+/**
+ * ADVANCED
+ * 1.) read the numbers from file1.txt
+ * 2.) read the numbers from file2.txt
+ * 3.) sort the combined list of all numbers in ascending order
+ * 4.) write the sorted numbers to out.txt
+ */
